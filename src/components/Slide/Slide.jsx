933b93fee4391713df1a790eed5ca696ccd90e80
plutoin <uleage@gmail.com>
2023-03-04 00:18:58 +0900

    Feat: Slide 기능 구현

--- src/components/Slide/Slide.jsx
+++ src/components/Slide/Slide.jsx
@@ -1,11 +1,50 @@
-import React from "react";
-import { Container, SwipeButton } from "./slide.style";
+import React, { useRef } from "react";
+import { SliderBox, PreDiv, NextDiv } from "./slide.style";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import { ReactComponent as Prev } from "../../assets/icon-swiper-1.svg";
+import { ReactComponent as Next } from "../../assets/icon-swiper-2.svg";
+
+import slide_hodu from "../../assets/image/001.png";
+import slide_spring from "../../assets/image/002.png";
+import slide_pet from "../../assets/image/003.png";
+import slide_coupon from "../../assets/image/004.png";
+import slide_card from "../../assets/image/005.png";
 
 export default function Slide() {
+  const images = useRef([
+    { src: slide_hodu, alt: "호두 마켓" },
+    { src: slide_spring, alt: "봄 세일" },
+    { src: slide_pet, alt: "반려동물 용품 세일" },
+    { src: slide_coupon, alt: "쿠폰 증정" },
+    { src: slide_card, alt: "카드 이벤트" },
+  ]);
+
+  const settings = {
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    prevArrow: (
+      <PreDiv>
+        <Prev />
+      </PreDiv>
+    ),
+    nextArrow: (
+      <NextDiv>
+        <Next />
+      </NextDiv>
+    ),
+  };
+
   return (
-    <Container>
-      <SwipeButton />
-      <SwipeButton />
-    </Container>
+    <SliderBox {...settings}>
+      {images.current.map((img, i) => (
+        <img key={i} src={img.src} alt={img.alt} />
+      ))}
+    </SliderBox>
   );
 }
--- src/components/Slide/slide.style.js
+++ src/components/Slide/slide.style.js
@@ -1,30 +1,28 @@
+import Slider from "react-slick";
 import styled from "styled-components";
-import leftArrow from "../../assets/icon-swiper-1.svg";
-import rightArrow from "../../assets/icon-swiper-2.svg";
 
-export const Container = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+export const SliderBox = styled(Slider)`
   padding-top: 94px;
-  height: 500px;
-  background-color: var(--color-bg);
-  box-sizing: border-box;
+  .slick-prev::before,
+  .slick-next::before {
+    opacity: 0;
+    display: none;
+  }
+  .slick-slide img {
+    cursor: pointer;
+  }
 `;
 
-export const SwipeButton = styled.button`
-  width: 60px;
-  height: 124px;
-  border: none;
-  background-color: transparent;
-  :first-child {
-    background-image: url(${leftArrow});
-    background-size: contain;
-    background-repeat: no-repeat;
-  }
-  :last-child {
-    background-image: url(${rightArrow});
-    background-size: contain;
-    background-repeat: no-repeat;
-  }
+export const PreDiv = styled.div`
+  width: 30px;
+  height: 30px;
+  margin-left: 30px;
+  z-index: 10;
+`;
+
+export const NextDiv = styled.div`
+  width: 30px;
+  height: 30px;
+  margin-right: 60px;
+  z-index: 10;
 `;
