f7cc4ecd7c88af66793560dc564975fede962fef
plutoin <uleage@gmail.com>
2023-04-10 23:17:38 +0900

    Feat: 판매자 상품 업로드 페이지 퍼블리싱

--- src/pages/ProductUpload/ProductUpload.jsx
+++ src/pages/ProductUpload/ProductUpload.jsx
@@ -1,6 +1,114 @@
-import React from "react";
-import {} from "./productUpload.style";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import SellerHeader from "../../components/Header/SellerHeader";
+import {
+  Section,
+  Container,
+  TextBox,
+  Form,
+  TopSection,
+  ImageWrapper,
+  ImgUploadBtn,
+  InputWrapper,
+  ShippingBtn,
+  DetailWrapper,
+  CharacterSpan,
+  UnitSpan,
+  ButtonContainer,
+  CancelBtn,
+  SaveBtn,
+} from "./productUpload.style";
 
 export default function ProductUpload() {
-  return <div>ProductUpload</div>;
+  const navigate = useNavigate();
+  const [isSelected, setIsSelected] = useState(true);
+
+  const checkedMethod = () => {
+    setIsSelected(!isSelected);
+  };
+
+  return (
+    <>
+      <SellerHeader />
+      <Section>
+        <h1>상품 등록</h1>
+        <Container>
+          <TextBox>
+            <p>* 상품 등록 주의사항</p>
+            <div>
+              - 상품명은 20자까지 입력 가능합니다.
+              <br />
+              <br />
+              - 상품 이미지 등록 시 jpg, png, gif 형식만 업로드 가능합니다.
+              <br />
+              <br />
+              - 재고는 0개로 설정할 수 없습니다.
+              <br />
+              <br />
+              - 기본 배송비를 0원으로 설정 시 무료배송으로 등록됩니다.
+              <br />
+              <br />- 각 필드는 필수 입력 정보입니다. 모든 상품 정보가
+              입력되어야 정상적으로 등록됩니다.
+            </div>
+          </TextBox>
+          <Form>
+            <TopSection>
+              <ImageWrapper>
+                <label htmlFor="productImage">상품 이미지</label>
+                <div>
+                  <ImgUploadBtn />
+                </div>
+              </ImageWrapper>
+
+              <InputWrapper>
+                <label htmlFor="productName">상품명</label>
+                <input type="text" id="productName" />
+                <CharacterSpan>0 / 20</CharacterSpan>
+
+                <label htmlFor="productPrice">판매가</label>
+                <input type="number" id="productPrice" />
+                <UnitSpan>원</UnitSpan>
+
+                <p>배송 방법</p>
+                <ShippingBtn
+                  name="shipping_method"
+                  id="shipping_method"
+                  isSelected={isSelected}
+                  onClick={() => checkedMethod()}
+                >
+                  택배, 소포, 등기
+                </ShippingBtn>
+                <ShippingBtn
+                  name="shipping_method"
+                  id="shipping_method"
+                  isSelected={!isSelected}
+                  onClick={() => checkedMethod()}
+                >
+                  직접배송(화물배달)
+                </ShippingBtn>
+
+                <label htmlFor="shippingFee">기본 배송비</label>
+                <input type="number" id="shippingFee" />
+                <UnitSpan>원</UnitSpan>
+
+                <label htmlFor="productStock">재고</label>
+                <input type="number" id="productStock" />
+                <UnitSpan>개</UnitSpan>
+              </InputWrapper>
+            </TopSection>
+            <DetailWrapper>
+              <label htmlFor="productDetail">상품 상세 정보</label>
+              <textarea type="text" id="productDetail" />
+            </DetailWrapper>
+          </Form>
+        </Container>
+
+        <ButtonContainer>
+          <CancelBtn onClick={() => navigate(-1)}>취소</CancelBtn>
+          <SaveBtn>저장</SaveBtn>
+        </ButtonContainer>
+      </Section>
+    </>
+  );
 }
--- src/pages/ProductUpload/productUpload.style.js
+++ src/pages/ProductUpload/productUpload.style.js
@@ -1,1 +1,188 @@
 import styled from "styled-components";
+
+import Image_Icon from "../../assets/icon-img.png";
+
+export const Section = styled.section`
+  width: 1920px;
+  padding: 44px 100px;
+  box-sizing: border-box;
+  > h1 {
+    margin-bottom: 42px;
+    font-size: 36px;
+    font-weight: 700;
+    line-height: 44px;
+  }
+`;
+
+export const Container = styled.div`
+  display: flex;
+  width: 100%;
+  gap: 80px;
+`;
+
+export const TextBox = styled.div`
+  > p {
+    color: #eb5757;
+    font-size: 16px;
+    font-weight: 500;
+    line-height: 20px;
+  }
+  > div {
+    width: 320px;
+    margin-top: 10px;
+    padding: 20px;
+    background: #ffefe8;
+    border-radius: 5px;
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 18px;
+    box-sizing: border-box;
+  }
+`;
+
+export const Form = styled.div`
+  display: flex;
+  width: 1320px;
+  flex-direction: column;
+  box-sizing: border-box;
+  label,
+  p {
+    display: block;
+    margin-bottom: 10px;
+    color: var(--color-deep-gray);
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 20px;
+  }
+  input,
+  textarea {
+    width: 220px;
+    margin-bottom: 16px;
+    padding: 16px;
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 20px;
+    border: 1px solid var(--color-light-gray);
+    border-radius: 5px;
+    box-sizing: border-box;
+  }
+  input[type="number"]::-webkit-outer-spin-button,
+  input[type="number"]::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+`;
+
+export const TopSection = styled.div`
+  display: flex;
+  gap: 40px;
+`;
+
+export const ShippingBtn = styled.button`
+  width: 220px;
+  margin-bottom: 16px;
+  margin-right: 10px;
+  padding: 16px 0;
+  border: ${(props) =>
+    props.isSelected ? "none" : "1px solid var(--color-light-gray)"};
+  border-radius: 5px;
+  color: ${(props) => (props.isSelected ? "white" : "var(--color-deep-gray)")};
+  background-color: ${(props) =>
+    props.isSelected ? "var(--color-green)" : "white"};
+  font-size: 16px;
+  font-weight: 500;
+  line-height: 20px;
+  box-sizing: border-box;
+`;
+
+export const ImageWrapper = styled.div`
+  > div {
+    width: 454px;
+    height: 454px;
+    background: #c4c4c4;
+  }
+`;
+
+export const ImgUploadBtn = styled.button`
+  position: relative;
+  top: 202px;
+  left: 202px;
+  width: 50px;
+  height: 50px;
+  border: none;
+  background: url(${Image_Icon}) no-repeat center / contain;
+`;
+
+export const InputWrapper = styled.div`
+  width: 100%;
+  #productName {
+    width: 100%;
+  }
+`;
+
+export const CharacterSpan = styled.span`
+  display: inline-block;
+  width: 46px;
+  margin-left: -67px;
+  color: #c4c4c4;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 18px;
+  text-align: right;
+  box-sizing: border-box;
+`;
+
+export const UnitSpan = styled.span`
+  display: inline-block;
+  width: 54px;
+  margin-left: -54px;
+  padding: 17px;
+  border-radius: 0px 5px 5px 0;
+  background-color: var(--color-light-gray);
+  color: white;
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 20px;
+  text-align: center;
+  box-sizing: border-box;
+`;
+
+export const DetailWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  margin-top: 40px;
+  > textarea {
+    width: 100%;
+    height: 700px;
+    resize: none;
+    outline: none;
+  }
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  gap: 14px;
+  float: right;
+  margin-top: 50px;
+  margin-bottom: 136px;
+  > button {
+    width: 200px;
+    padding: 19px 0;
+    border-radius: 5px;
+    font-size: 18px;
+    font-weight: 700;
+    line-height: 22px;
+  }
+`;
+
+export const CancelBtn = styled.button`
+  border: 1px solid var(--color-light-gray);
+  background-color: white;
+`;
+
+export const SaveBtn = styled.button`
+  border: none;
+  color: white;
+  background-color: var(--color-green);
+`;
